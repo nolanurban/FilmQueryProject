076586ac372cc05f6a98b84df41d3f2e12a233df
Nolan Urban <nolanurban@gmail.com>
2022-10-02 16:25:46 -0400

    Program seems to be working now.  Still concerned about leaving open connections, statements, and resultsets -- however the code is finding it.  I still need to parse for doubles as the film list is designed to take more on for other fields.

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -8,52 +8,54 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
- // DatabaseAccessor db = new DatabaseAccessorObject();
-DatabaseAccessorObject db = new DatabaseAccessorObject();
 
-  public static void main(String[] args) throws SQLException {
-	String dummyTest = "select title, release_year, rating, description, length from film";
-	FilmQueryApp app = new FilmQueryApp();
-    app.launch();
- //   app.test();
- //   System.out.println(db.findFilmById(143).toString()); 
-//    app.launch();
-//    String checkResultTest = "select actor_id, actor.first_name, actor.last_name, film.title, film.id, film.release_year, film.rating, film.description, film.length, language.name from film_actor join actor on film_actor.actor_id = actor.id join film on film_actor.film_id = film.id join language on film.language_id = language.id";
-//    db.databaseAccess(checkResultTest);
-    
-  }
+	// DatabaseAccessor db = new DatabaseAccessorObject();
+	DatabaseAccessorObject db = new DatabaseAccessorObject();
 
-//  private void test() {
-//    Film film = db.findFilmById(1);
-//    System.out.println(film);
-//  }
-//
-  private void launch() throws SQLException {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
+	public static void main(String[] args) throws SQLException {
 
-  private void startUserInterface(Scanner input) throws SQLException {
-    System.out.println("Welcome to our last use of Scanner\n");
-    System.out.println("Please selet from an option below: ");
-    System.out.println("1. Look up a film by it's ID number");
-    System.out.println("2. Look up a film by searching a keyword");
-    System.out.println("3. Exit the application");
-    
-    int optionNumber = input.nextInt();
-    switch (optionNumber) {
-    case 1: System.out.println("Please enter a film number ID to look up: ");
-    	int lookupNumber = input.nextInt();
-        System.out.println(db.findFilmById(lookupNumber).toString()); 
-        break;
-    case 2:
-    case 3: break;
-    default: System.out.println("Error");
-    }
-  }
+		FilmQueryApp app = new FilmQueryApp();
+		app.launch();
+	}
+
+	private void launch() throws SQLException {
+		Scanner input = new Scanner(System.in);
+
+		startUserInterface(input);
+
+		input.close();
+	}
+	private void printMenu() {
+		System.out.println("Welcome to our last use of Scanner\n");
+		System.out.println("Please selet from an option below: ");
+		System.out.println("1. Look up a film by it's ID number");
+		System.out.println("2. Look up a film by searching a keyword");
+		System.out.println("3. Exit the application");
+
+	}
+	private void startUserInterface(Scanner input) throws SQLException {
+		printMenu();
+		int optionNumber = input.nextInt();
+			switch (optionNumber) {
+			case 1:
+				System.out.println("Please enter a film number ID to look up: ");
+				int lookupNumber = input.nextInt();
+				System.out.println(db.findFilmById(lookupNumber).toString());
+				break;
+			case 2:
+				System.out.println("Please enter a keyword to search for in the film titles");
+				String keyWord = input.next();
+				for (Film f : db.findFilmBySearchString(keyWord)) {
+					if (f.equals(null)) System.out.println("Your search: " + keyWord + " was not found");
+					System.out.println(f);
+					
+				}
+			case 3:
+				break;
+			default:
+				System.out.println("Error");
+			}
+		
+	}
 
 }
--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -46,6 +46,29 @@ static { // driver access
     return film;
     
   }
+  public List<Film> findFilmBySearchString(String searchString) throws SQLException {
+		String SQL = "select actor_id, actor.first_name, actor.last_name, film.title, film.id, film.release_year, film.rating, film.description, film.length, language.name from film_actor join actor on film_actor.actor_id = actor.id join film on film_actor.film_id = film.id join language on film.language_id = language.id where film.title like '%" + searchString + "%' or description like '%" + searchString + "%'";
+		ResultSet result = databaseAccess(SQL);
+		List<Actor> actorList = new ArrayList<>();  // placed for long term goal
+	    List<Film> searchList = new ArrayList<>();
+ 		
+	    if (result.next() == false) 
+	    	System.out.println("No such key word exists.");
+	    while (result.next()) {
+	    		Film film = new Film(result.getInt("id"), 
+							result.getString("title"), 
+							result.getInt("release_year"),
+							result.getString("rating"),
+							result.getString("description"),
+							result.getString("name"), //language
+							result.getInt("length"),
+							actorList);
+	    		searchList.add(film);
+	    		}   
+	    return searchList;
+	    
+}
+
   
 public ResultSet databaseAccess(String input) throws SQLException {
 		String user = "student";
@@ -55,25 +78,6 @@ public ResultSet databaseAccess(String input) throws SQLException {
 		PreparedStatement stmt = conn.prepareStatement(input);
 		ResultSet result = stmt.executeQuery();
 		
-//			while (result.next()) {
-//				if (result.getInt("id") == 1) {
-//			   					System.out.println(result.getString("title")); 
-//			   					System.out.println(result.getInt("release_year"));
-//			   					System.out.println(result.getString("rating"));
-//			   					System.out.println(result.getString("description"));
-//			   					System.out.println(result.getString("name")); //language
-//			   					System.out.println(result.getInt("length"));
-//				}
-//			}
-////		int customerCount = 0;
-////		while (result.next() && customerCount != 20) {
-////			customerCount++;
-////			System.out.println(result.getString("first_name") + " " + result.getString("last_name"));
-////		}
-//		stmt.close();
-//		result.close();
-//		conn.close();
-//		System.out.println(result.getMetaData());
 		return result;
 		
 	}
